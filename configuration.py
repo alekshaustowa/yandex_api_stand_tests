ce026a305049e3fc344b93c59ecf40693772f01d
unknown <aleksandrahaustova4@gmail.com>
2024-02-18 20:15:36 +0500

    first

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://30251261-b703-4a15-8834-edc06bc7cb93.serverhub.praktikum-services.ru'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = '/api/db/resources/user_model.csv'
+CREATE_USER_PATH = "/api/v1/users/"
+MAIN_PRODUCTS_PATH = '/api/v1/products/kits'
